0af23dd9b2df8b056d7143bdb92b09f55af32a37
lshug <lshugliashvili4659@sdsu.edu>
2020-10-29 22:11:34 +0400

    Added text corpus generation and shuffle order to Iverieli scraper

--- iverieli/get_iverieli.sh
+++ iverieli/get_iverieli.sh
@@ -4,7 +4,5 @@ python3 scrap_pdf_urls.py
 echo "Converting PDFs to images..."
 python3 convert_pdfs.py
 echo "Extracting characters..."
-python3 extract.py
-echo "Shuffling extracted characters..."
 python3 shuffle.py
 echo "Done."
