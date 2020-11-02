ebb88d2e0ac8e34e8f7833c58d966b3a14353bd5
lshug <levan@shugliashvili.com>
2020-11-02 19:15:19 +0400

    Added dataloader

--- server/dataloader.py
+++ server/dataloader.py
@@ -1,16 +1,53 @@
+import re
 import os
+from tqdm import tqdm
+from PIL import Image
+from tesserocr import PyTessBaseAPI, RIL
+from pdf2image import convert_from_path
+from utils import segment, refine_boxes
+from bs4 import BeautifulSoup
+
+punctuations = list("'" + '.,"`_-/\\?!–’—”„%()')
+
+def process_hocr(hocr, img = None):
+    pars_out = []
+    soup = BeautifulSoup(hocr)
+    paragraphs = soup.body.find_all('p', attrs={'class':'ocr_par'})
+    for p in tqdm(paragraphs, 'Processing paragraphs'):
+        p_box = tuple([int(x) for x in p.attrs['title'].split(';')[0].split(' ')[1:]])
+        words_out = []
+        words = p.find_all('span', attrs={'class':'ocrx_word'})
+        for w in tqdm(words, 'Processing words'):
+            w_box = tuple([int(x) for x in w.attrs['title'].split(';')[0].split(' ')[1:]])
+            chars_out = []
+            chars = w.find_all('span', attrs={'class':'ocrx_cinfo'})
+            for c in tqdm(chars, 'Processsing characters'):
+                c_box = tuple([int(x) for x in c.attrs['title'].split(';')[0].split(' ')[1:]])
+                c_label = c.text
+                c_label = c_label if c_label in punctuations else ''
+                chars_out.append({'box':c_box, 'label':c_label})
+            words_out.append({'box':p_box, 'chars':chars_out})
+        pars_out.append({'box':p_box, 'words':words_out})
+    return pars_out
 
 def process_pdf(path):
-    #extract images
-    #delete delete pdf
-    #run process_images
-    pass
+    pdf_file = path + '/' + os.listdir(path)[0]
+    convert_from_path(pdf_file, fmt='png', output_folder=path)
+    return process_images(path)
 
 def process_images(path):
-    #for each image:
-    #   Run full tesseract symbol level
-    #   Run symbol-level boxes through erosion check
-    #   Run tesseract layout analsis on above levels
-    #   Remove labels for everything that's not punctuation
-    #   Make the full document hierarchy json by checking boundaries.
-    #List of (img,json)
+    os.chdir(path)
+    img_paths, page_jsons = [], []
+    for img_path in tqdm([f for f in os.listdir() if '.png' in f], 'Processing images'):
+        img = Image.open(img_path)
+        with PyTessBaseAPI(psm=3) as api:
+            api.SetVariable('hocr_char_boxes', 'true')
+            api.SetImage(img)
+            api.Recognize()
+            hocr = api.GetHOCRText(0)
+        page_json = process_hocr(hocr, img)
+        img_paths.append(img_path)
+        page_jsons.append(page_json)
+    return img_paths, page_jsons
+
+    
--- server/utils.py
+++ server/utils.py
@@ -1,33 +1,36 @@
 from PIL import Image, ImageFilter
 import numpy as np
+from scipy.ndimage import maximum_filter
 from tesserocr import PyTessBaseAPI, RIL
+from tqdm import tqdm
 
-
+def segment(img, psm=7, ril=RIL.SYMBOL):
+    with PyTessBaseAPI(psm=psm) as api:
+        boxes = []
+        api.SetImage(img)
+        img_iter = api.AnalyseLayout()
+        if img_iter is None:
+            return boxes
+        has_next = True
+        while has_next:
+            box = img_iter.BoundingBox(ril)
+            if box is not None:
+                boxes.append(box)
+            has_next = img_iter.Next(ril)
+    return boxes
 
 def erode(im, size=3):
     im = Image.fromarray(im)
     im = im.filter(ImageFilter.MaxFilter(size))
     return im
 
-
-def resegment(img, psm=7):
-    with PyTessBaseAPI(psm=psm) as api:
-        boxes = []
-        api.SetImage(img)
-        img_iter = api.AnalyseLayout()
-        boxes.append(img_iter.BoundingBox(RIL.SYMBOL))
-        while img_iter.Next(RIL.SYMBOL):
-            boxes.append(img_iter.BoundingBox(RIL.SYMBOL))
-    return boxes
-
-
 def refine_boxes(img, boxes):
     good_boxes, bad_boxes = filter_boxes(boxes)
     img_np = np.asarray(img)
-    for x, y, xw, yh in bad_boxes:
+    for x, y, xw, yh in tqdm(bad_boxes, 'Resegmenting bad boxes'):
         im_temp = img_np[y : yh, x : xw]
         im_temp = erode(im_temp)
-        new_boxes = resegment(im_temp)
+        new_boxes = segment(im_temp)
         new_boxes_rebased = [(box[0]+x, box[1]+y, box[2]+x, box[3]+y) for box in new_boxes]
         good_boxes = good_boxes + new_boxes_rebased
     return good_boxes
@@ -36,7 +39,7 @@ def refine_boxes(img, boxes):
 def filter_boxes(boxes):
     good_boxes = []
     bad_boxes = []
-    for box in boxes:
+    for box in tqdm(boxes, 'Identifying bad boxes'):
         x, y, xw, yh = box
         w,h = x - xw, y - yh
         ratio = w / h
