7b630a1a8b3c91387c88ef18aa1ad290ba80bed3
lshug <levan@shugliashvili.com>
2020-11-05 23:34:45 +0400

    Updated missing model file case

--- server/model_serving.py
+++ server/model_serving.py
@@ -51,11 +51,11 @@ LABEL_CHARS = [
     "ჰ",
 ]
 LABEL_ENCODINGS = dict(enumerate(LABEL_CHARS))
-if not os.path.isfile(f"{os.path.dirname(__file__)}/model.h5"):
+if os.path.isfile(f"{os.path.dirname(__file__)}/model.h5"):
+    model = keras.models.load_model(f"{os.path.dirname(__file__)}/model.h5")
+else:    
     model = keras.applications.ResNet152V2(include_top=True, weights=None, input_shape=(32,32,1), classes=len(LABEL_CHARS))
     model.predict(np.random.random_sample((1,32,32,1)))
-
-model = keras.models.load_model(f"{os.path.dirname(__file__)}/model.h5")
 
 def infer(img):
     img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (32, 32)).reshape((32, 32, 1))[None]
