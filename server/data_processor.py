6338dabe801ee96c52a2cb9ed1078b293ebb421f
lshug <levan@shugliashvili.com>
2020-11-27 00:35:26 +0400

    Removed uncommon character inferences

--- server/data_processor.py
+++ server/data_processor.py
@@ -21,32 +21,29 @@ punctuations = list("'" + '.,"`_-/\\?!–’—”„%()')
 LABEL_CHARS = list('0123456789?აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ')
 LABEL_ENCODINGS = dict(enumerate(LABEL_CHARS))
 
-
-#@worker_process_init.connect()
-#def worker_init(**_):
-#    global model
-#    physical_devices = tf.config.list_physical_devices('GPU')
-#    for device in physical_devices:
-#        try:
-#            tf.config.experimental.set_memory_growth(device, True)
-#        except:
-#            pass
-#    if os.path.isfile(f"{os.path.dirname(__file__)}/model.h5"):
-#        model = keras.models.load_model(f"{os.path.dirname(__file__)}/model.h5")
-#    else:
-#        model = keras.applications.ResNet152V2(include_top=True, weights=None, input_shape=(32,32,1), classes=len(LABEL_CHARS))
-#        model.predict(np.random.random_sample((1,32,32,1))) # ensure weight initialization
-#        
-#
-## get this into the worker init func
-#
-#    
-#def predict(img):
-#    img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (32, 32)).reshape((32, 32, 1))[None]
-#    prediction = model.predict(img)
-#    label = LABEL_ENCODINGS[np.argmax(prediction[0])]
-#    return label
-
+'''
+@worker_process_init.connect()
+def worker_init(**_):
+    global model
+    physical_devices = tf.config.list_physical_devices('GPU')
+    for device in physical_devices:
+        try:
+            tf.config.experimental.set_memory_growth(device, True)
+        except:
+            pass
+    if os.path.isfile(f"{os.path.dirname(__file__)}/model.h5"):
+        model = keras.models.load_model(f"{os.path.dirname(__file__)}/model.h5")
+    else:
+        model = keras.applications.ResNet152V2(include_top=True, weights=None, input_shape=(32,32,1), classes=len(LABEL_CHARS))
+        model.predict(np.random.random_sample((1,32,32,1))) # ensure weight initialization
+        
+    
+def predict(img):
+    img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (32, 32)).reshape((32, 32, 1))[None]
+    prediction = model.predict(img)
+    label = LABEL_ENCODINGS[np.argmax(prediction[0])]
+    return label
+'''
 
 def process_hocr(hocr, img, page, tess_mode=True):
     img_np = np.asarray(img)
