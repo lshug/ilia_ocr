e3591600f3fff3c8c79f2ae49d7fd3bac244d27b
lshug <levan@shugliashvili.com>
2021-11-17 19:06:15 +0400

    Wait for database launch

--- server/database.py
+++ server/database.py
@@ -1,12 +1,17 @@
 from databases import Database
 from sqlalchemy import create_engine, Column, Integer, String, LargeBinary, MetaData, Table
+from sqlalchemy_utils.functions import database_exists
 from .settings import settings
+from time import sleep
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
 connect_args = {}
 
 if 'sqlite://' in SQLALCHEMY_DATABASE_URL:
     connect_args["check_same_thread"] = False
+
+while not database_exists(SQLALCHEMY_DATABASE_URL):
+    sleep(1)
 
 database = Database(SQLALCHEMY_DATABASE_URL)
 
