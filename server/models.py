9ba6ee50b780a1eef7ec88d9ca575152efd123b5
lshug <levan@shugliashvili.com>
2021-11-18 13:04:28 +0400

    Make file contents retrieval synchronous when using postgres

--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List, Tuple
 from .server_utils import redis_session, get_random_string
 from .database import database, raw_files, is_bootstrapping
+import psycopg2
 import asyncio
 import os
 
@@ -49,10 +50,25 @@ def new_document_id():
     redis_session.lpush('documents', new_id)
     return new_id
 
-async def retrieve_raw_file(id):
+async def async_retrieve_raw_file(id):
     await connect_db()
     query = raw_files.select().where(raw_files.c.id==id)
     return (await database.fetch_all(query))[0]
+
+def postgresql_retrieve_raw_file_contents(id):
+    assert type(id) is int
+    connection = psycopg2.connect(user=settings.database_username,
+                                  password=settings.database_password,
+                                  host=settings.database_host,
+                                  port="5432",
+                                  database=settings.database_name)
+    cursor = connection.cursor()
+    select_query = f'SELECT contents FROM raw_files WHERE id={id}'
+    cursor.execute(select_query)
+    contents = cursor.fetchone()[0]
+    cursor.close()
+    connection.close()
+    return contents
         
 async def insert_raw_file(filename, mime_type, contents):
     query = raw_files.insert().values(filename=filename, mime_type=mime_type, contents=contents)
